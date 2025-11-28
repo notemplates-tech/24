8e1e78085226cdecb4217c94052d9a1c5b2dd8ca
notemplates-tech <notemplates@gmail.com>
2025-11-28 18:24:45 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '')
   return {
-    base: '/24mountains/',  // ← имя вашего репозитория!
+    base: '/24/',  // ← имя вашего репозитория!
     server: {
       port: 3000,
       host: '0.0.0.0',
